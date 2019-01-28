79af766ca0fabfc36868c93071fbe81542ad7234
Agy Pascha <43352495+kazoeru@users.noreply.github.com>
2019-01-28 13:32:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def handle_message(event):
         message = TemplateSendMessage(
             alt_text = "SeGame Musik",
             template = CarouselTemplate(
-                columns = [ datalagu ]
+                columns = [ (datalagu) ]
             )
         )
         line_bot_api.reply_message(event.reply_token, message)  
