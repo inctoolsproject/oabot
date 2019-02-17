b4396c6a29e61dce6e6ce9ede598f11f057ed215
Agy Pascha <43352495+kazoeru@users.noreply.github.com>
2019-02-18 03:27:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -589,7 +589,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)  
 
 
-    elif "https://api.boteater.co/joox/single/"
+    # elif "https://api.boteater.co/joox/single/"
 #=====[ FLEX MESSAGE ]==========
     # elif text == "yud test":
     #     message = ImagemapSendMessage(
